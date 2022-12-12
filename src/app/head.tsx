49377acada43752574220fa58365641af0939c6d
Robert Koch <robert@kochie.io>
2022-12-12 14:20:42 +1100

    removes page for testing

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,3 +1,5 @@
+import { NextSeo } from "next-seo";
+
 export default function Head() {
   return (
     <>
@@ -5,6 +7,35 @@ export default function Head() {
       <link rel="manifest" href="/manifest.json" />
       <meta charSet="utf-8" />
       <link rel="icon" type="image/png" href="/favicon.png" />
+
+      <NextSeo
+        useAppDir={true}
+        canonical="https://touch-typer.kochie.io"
+        title="Touch Typer"
+        description="Touch Typer is an app that lets you practice your typing skills."
+        openGraph={{
+          type: "website",
+          images: [
+            {
+              url: "https://touch-typer.kochie.io/og.png",
+              height: 630,
+              width: 1200,
+              alt: "OpenGraph image",
+            },
+          ],
+          description:
+            "Touch Typer is an app that lets you practice your typing skills.",
+          siteName: "Touch Typer",
+          url: "https://touch-typer.kochie.io",
+          title: "Touch Typer",
+        }}
+        twitter={{
+          handle: "@kochie",
+          site: "@kochie",
+          cardType: "summary_large_image",
+        }}
+        themeColor="#42444D"
+      />
     </>
   );
 }
