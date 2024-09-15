bd8ebbcab1bff00c939daf2b9f1dcc3db70d499d
Robert Koch <robert@kochie.io>
2024-09-16 02:12:04 +1000

    Remove enums and unnecessary imports in leaderboard page

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import LeaderboardSection from "@/components/LeaderboardSection";
+import { KeyboardLayoutNames, Languages, Levels } from "@/utils/enums";
 import { Field, Fieldset, Label, Select } from "@headlessui/react";
 import { useReducer } from "react";
 
@@ -37,27 +38,6 @@ function reducer(state: State, action: Actions) {
   }
 }
 
-export enum KeyboardLayoutNames {
-  MACOS_US_QWERTY = "MACOS_US_QWERTY",
-  MACOS_US_DVORAK = "MACOS_US_DVORAK",
-  MACOS_US_COLEMAK = "MACOS_US_COLEMAK",
-  MACOS_FR_AZERTY = "MACOS_FR_AZERTY",
-  MACOS_DE_QWERTZ = "MACOS_DE_QWERTZ",
-  MACOS_ES_QWERTY = "MACOS_ES_QWERTY",
-}
-
-export enum Levels {
-  LEVEL_1 = "1",
-  LEVEL_2 = "2",
-  LEVEL_3 = "3",
-}
-
-export enum Languages {
-  ENGLISH = "en",
-  FRENCH = "fr",
-  GERMAN = "de",
-  SPANISH = "es",
-}
 
 export default function LeaderboardPage() {
   const [{ keyboard, language, level }, dispatch] = useReducer(reducer, {
